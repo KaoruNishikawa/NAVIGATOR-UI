e9dfda2c36d1504d0b54546b6beeb5dd736ac4d0
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-06-14 22:40:27 +0900

    #2 remove server side or python related scripts

--- start_server.sh
+++
@@ -1,7 +0,0 @@
-# specify server IP address and port if given as args
-if [ $# -ne 0 ]; then
-    export NASCO_SERVER_IP=$1
-    if [ $# -ge 2 ]; then
-        export NASCO_SERVER_PORT=$2
-# start the server
-node server.js
