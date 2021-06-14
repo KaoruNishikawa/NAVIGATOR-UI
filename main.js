bd657f46fbb51517e1fd825d9ba16b94a12e6d9f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-06-14 23:14:05 +0900

    #2 add main script

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            contextIsolation: true
+        }
+    })
+    win.loadFile("src/static/launch.html")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on("active", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
