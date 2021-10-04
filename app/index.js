859d02086dcbf4d299c50d6018f507c7f6cb9ed7
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-10-04 11:29:31 +0900

    #13 place index files in app directory

---
+++ app/index.js
@@ -0,0 +1,45 @@
+"use strict"
+
+const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // icon:
+        show: false,
+        paintWhenInitiallyHidden: true,
+        backgroundColor: "#FFF",
+        webPreferences: {
+            nodeIntegration: true,  // *1
+            sandbox: false,  // *1
+            contextIsolation: false,  // *1
+            nativeWindowOpen: true
+        }
+        /*
+         *1: Only this setting allows the scripts which are loaded on
+             html files to use CommonJS.
+        */
+    })
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show()
+    })
+
+    mainWindow.loadFile(path.join(__dirname, "hotkey", "hotkey.html"))
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
