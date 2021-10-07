37943171f94d5705ae30bb5f6eda899d996e5a79
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-10-07 12:28:57 +0900

    #2 Add required entries

--- app/Splash/splash.js
+++ app/Splash/splash.js
@@ -1,4 +1,11 @@
 "use strict"
 
-// button event
+for (const packageName of ["node", "chrome", "electron"]) {
+    const element = document.getElementById(`${packageName}-version`)
+    element.innerText = process.versions[packageName]
+}
+
+
+
+// catch button event
 /* main.setParam("serverIP", input) */
